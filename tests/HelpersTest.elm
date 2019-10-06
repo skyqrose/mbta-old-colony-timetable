9abd7ebfbc13a208553a43654e6c09b58faee088
Sky Rose <zgnomdnv@sky.skyqrose.com>
2019-10-06 18:14:26 -0400

    git submodule update elm-mbta-api

--- tests/HelpersTest.elm
+++ tests/HelpersTest.elm
@@ -10,7 +10,7 @@ suite =
     describe "Helpers"
         [ describe "uniq"
             [ test "removes duplicates" <|
-                \_ ->
+                \() ->
                     Expect.equal
                         (Helpers.uniq [ 1, 3, 2, 1, 2 ])
                         [ 1, 3, 2 ]
